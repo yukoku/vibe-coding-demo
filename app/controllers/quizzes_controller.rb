d4fb842fdd609d44e73e3f09c3515c0bfb020bfd
yukoku <powerpythoncc@gmail.com>
2025-09-22 16:54:48 +0900

    feat: Implement Quiz CRUD and add Quiz model validations

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,50 @@
+class QuizzesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_quiz, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @quizzes = Quiz.all
+  end
+
+  def show
+  end
+
+  def new
+    @quiz = current_user.quizzes.build
+  end
+
+  def create
+    @quiz = current_user.quizzes.build(quiz_params)
+    if @quiz.save
+      redirect_to @quiz, notice: 'Quiz was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @quiz.update(quiz_params)
+      redirect_to @quiz, notice: 'Quiz was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @quiz.destroy
+    redirect_to quizzes_url, notice: 'Quiz was successfully destroyed.'
+  end
+
+  private
+
+  def set_quiz
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def quiz_params
+    params.require(:quiz).permit(:title, :description, :public)
+  end
+end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,7 @@ class Quiz < ApplicationRecord
   belongs_to :user
   has_many :questions
   has_many :results
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
